40463bf945dc23d4e3f3d3abd032dfab30994fdb
Yatheswar546 <yatheswar546@gmail.com>
2026-05-13 17:47:55 +0530

    running-notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import './App.css'
 
-import ExternalCSS from './concepts/24. ReactStyling/01. ExternalCSS/ExternalCSS';
-import InlineCSS from './concepts/24. ReactStyling/02. InlineCSS/InlineCSS';
-import DynamicStyle from './concepts/24. ReactStyling/02. InlineCSS/DynamicStyle';
-import Button from './concepts/24. ReactStyling/03. CSSModules/Button';
-import StyledComponent from './concepts/24. ReactStyling/04. CSSInJS/StyledComponent';
-
 export default function App() {
 
   return (
       <>
-        <ExternalCSS />
-        <InlineCSS />
-        <DynamicStyle />
-        <Button />
-        <StyledComponent />
+        <h1>Hello, Welcome to React</h1>
       </>
   );
 }
